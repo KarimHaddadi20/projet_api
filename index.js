39daf79da92bd37b316cc50c68fa30487eda9d0e
karim <haddadi.karim20@gmail.com>
2023-11-29 21:07:01 +0100

    rajout du routes/v1.js  sur les imports

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
+
 import express from "express";
 import cors from 'cors';
-import { PrismaClient } from '@prisma/client';
+import 'dotenv/config';
+import fs from "fs";
+import v1Router from "./routes/v1.js";
 
 
 const app = express();
 
-const prisma = new PrismaClient();
-
-
-
-
 app.use(express.json());
-
 app.use(
   cors({
     origin: ["http://localhost:5500"],
     credentials: true,
   })
 );
+app.use("/v1", v1Router);
+
+app.use((err, req, res, next) => {
+  if (err.name === "UnauthorizedError") {
+    return res.status(401).json({ msg: "Ton JWT est invalide !" });
+  }
+
+  console.log("erreur", "err");
+
+  return res.status(err.status).json({ message: err.message });
+});
 
 
 
