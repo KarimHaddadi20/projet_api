26427ed0956af045b176d9a53daec7deffbee6ae
karim <haddadi.karim20@gmail.com>
2023-11-30 17:45:45 +0100

    j'ai retirer le regex sur le password

--- validators/RegisterValidator.js
+++ validators/RegisterValidator.js
@@ -1,21 +1,13 @@
+//RegisterValidator.js
+
 import { z } from "zod";
 
 const RegisterValidator = z.object({
-  firstname: z.string().min(1).max(255),
-  lastname: z.string().min(1).max(255),
-  email: z.string().email(),
+  firstName: z.string(),
+  lastName: z.string(),
+  email: z.string(),
   password: z
     .string()
-    .min(6)
-    .regex(new RegExp(/[0-9]/), {
-      message: "le mdp doit contenir au moins un chiffre",
-    })
-    .regex(new RegExp(/[A-Z]/), {
-      message: "le mdp doit contenir au moins une majuscule",
-    })
-    .regex(new RegExp(/[a-z]/), {
-      message: "le mdp doit contenir au moins une minuscule",
-    }),
 });
 
 export default RegisterValidator;
